b36555257e18bba1de408ed8ee308beca4dfd5d4
user <andreasalucci98@gmail.com>
2026-01-07 11:19:00 +0000

    4

--- executor/execute___process_commands.c
+++ executor/execute___process_commands.c
@@ -4,6 +4,15 @@ void	process_commands(t_command *cmds, t_env **env, bool *hdc_interrupted)
 {
 	if (!cmds)
 		return ;
+	if (cmds->argv && cmds->argv[0] && !(cmds->argv[1]))
+	{
+		if (ft_strcmp(cmds->argv[0], "\0") == 0)
+		{
+			free_command_l(cmds);
+			g_exit_status = 127;
+			return ;
+		}
+	}
 	if (cmds->argv && cmds->argv[0])
 	{
 		if (first_arg_is_one_dot(cmds->argv[0])
